cdcc317463eee854669155e4e4b5bec3a3de44eb
Jaurron Crawford <jaurroncrawford@yahoo.com>
2017-07-10 13:37:59 -0500

    Initial Commit

---
+++ src/main/java/org/launchcode/clique/CliqueApplication.java
@@ -0,0 +1,14 @@
+package org.launchcode.clique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliqueApplication.class, args);
+		System.out.println("Hello World");
+
+	}
+}
